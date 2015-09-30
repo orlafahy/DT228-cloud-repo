a6d33c6041f6746ee7702d7551d3b882c0de53f3
orlafahy <c13520633@mydit.ie>
2015-09-30 15:15:09 +0000

    Comments added

--- Lab3/plaindrome.py
+++ Lab3/plaindrome.py
@@ -1,8 +1,13 @@
+#Read in input from user
 a = input("What word would you like to check\n")
- 	
+
+#line to reverse the string that has been read in and saved as new string
 reverse_word = a[::-1]
  
+#checking if original string and reversed string are the same
 if reverse_word == a:
+#true printed if string is a palindrome
 	print(True)
 else:
+#false printed if string is not  a palindrome
 	print(False)
